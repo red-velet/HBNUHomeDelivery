8bc79c5a79a62250f0b674b77f02c45c0e26209c
chiou <bluebeastmight@gmail.com>
2023-04-01 19:39:48 +0800

    redis环境配置

---
+++ src/main/java/com/qxy/reggie/config/RedisConfig.java
@@ -0,0 +1,24 @@
+package com.qxy.reggie.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @Author: SayHello
+ * @Date: 2023/4/1 19:33
+ * @Introduction: redis模板配置类
+ */
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        //设置key的序列化器,不设置则会使用jdk默认的key序列花旗:JdkSerializationRedisSerializer
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setConnectionFactory(connectionFactory);
+        return redisTemplate;
+    }
+}
